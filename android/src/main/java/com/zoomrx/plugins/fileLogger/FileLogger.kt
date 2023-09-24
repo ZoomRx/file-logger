891600e9b9844684aabd1faa601ec7af4caabc91
Prem Kumar <premji012@gmail.com>
2023-09-24 15:07:37 +0530

    Added plugin functions for android

---
+++ android/src/main/java/com/zoomrx/plugins/fileLogger/FileLogger.kt
@@ -0,0 +1,51 @@
+package com.zoomrx.plugins.fileLogger
+
+import android.util.Log
+
+import com.getcapacitor.*
+
+@NativePlugin
+class FileLogger : Plugin() {
+
+    @PluginMethod
+    fun getLoggerIndex(call: PluginCall) {
+        Logger.index++
+        Log.d("object_check", Logger.index.toString())
+    }
+
+    @PluginMethod
+    fun log(call: PluginCall) {
+        val level = call.getString("level")
+        val message = call.getString("message")
+        var consoleFunc = level
+            when (level) {
+                "debug" -> {
+                    Logger.debug(message)
+                }
+                "error" -> {
+                    Logger.error(message)
+                }
+                "warn" -> {
+                    Logger.warn(message)
+                }
+                "info" -> {
+                    Logger.info(message)
+                }
+                else -> {
+                    Logger.verbose(message)
+                    consoleFunc = "log"
+                }
+            }
+        bridge.webView.evaluateJavascript("console.$consoleFunc((\"${message}\"))", null)
+        val ret = JSObject()
+        ret.put("success", true)
+        call.success(ret)
+    }
+
+    @PluginMethod
+    fun getLogDirPath(call: PluginCall) {
+        val ret = JSObject()
+        ret.put("path", "${context.filesDir.absolutePath}/logs")
+        call.success(ret)
+    }
+}
